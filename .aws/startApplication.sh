34682b9976e6a43e6c6114db620b0d3a1c5ec6c9
Piercuta <pierre.courteille@gmail.com>
2021-12-16 18:07:34 +0100

    update startApplication.sh stopApplication.sh

--- .aws/startApplication.sh
+++ .aws/startApplication.sh
@@ -3,7 +3,7 @@
 # Start Application
 echo "Start Application"
 # Env=$(cat /tmp/env.txt)
-# cd /var/www/disquaire
-# pip3 install -r requirements.txt
-# python3 manage.py runserver 0.0.0.0:8000
+cd /var/www/disquaire
+pip3 install -r requirements.txt
+python3 manage.py runserver 0.0.0.0:8000 > /dev/null 2>&1 &
 
--- .aws/stopApplication.sh
+++ .aws/stopApplication.sh
@@ -3,5 +3,5 @@
 # Stop Application
 echo "Stop Application"
 # Env=$(cat /tmp/env.txt)
-# kill process
-# rm -rf /var/www/diquaire
+kill $(ps aux | grep runserver | awk '{print $2}')
+rm -rf /var/www/diquaire
